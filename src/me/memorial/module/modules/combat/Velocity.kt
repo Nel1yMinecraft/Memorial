562427d241350a7640e5dae4e46f47061c17e0c8
Itzdudu1337 <121291861+Itzdudu1337@users.noreply.github.com>
2023-10-03 13:51:47 +0800

    Grim velocity
    L

--- src/me/memorial/module/modules/combat/Velocity.kt
+++ src/me/memorial/module/modules/combat/Velocity.kt
@@ -1,208 +1,115 @@
 package me.memorial.module.modules.combat
 
 import me.memorial.Memorial
-import me.memorial.events.EventTarget
-import me.memorial.events.JumpEvent
-import me.memorial.events.PacketEvent
-import me.memorial.events.UpdateEvent
+import me.memorial.events.*
+import me.memorial.events.impl.player.MotionEvent
 import me.memorial.module.Module
 import me.memorial.module.ModuleCategory
 import me.memorial.module.ModuleInfo
-import me.memorial.module.modules.movement.Speed
-import me.memorial.utils.MovementUtils
-import me.memorial.utils.timer.MSTimer
+import me.memorial.module.modules.player.Blink
 import me.memorial.value.BoolValue
 import me.memorial.value.FloatValue
-import me.memorial.value.ListValue
-import net.minecraft.network.play.server.S12PacketEntityVelocity
-import net.minecraft.network.play.server.S27PacketExplosion
-import net.minecraft.util.MathHelper
+import me.memorial.value.IntegerValue
 
-@ModuleInfo(name = "Velocity", description = "Allows you to modify the amount of knockback you take.", category = ModuleCategory.COMBAT)
+import net.minecraft.network.play.client.C03PacketPlayer
+import net.minecraft.network.play.client.C03PacketPlayer.*
+import net.minecraft.network.play.client.C07PacketPlayerDigging
+import net.minecraft.network.play.server.S12PacketEntityVelocity
+import net.minecraft.util.BlockPos
+import net.minecraft.util.EnumFacing
+
+@ModuleInfo(name = "Velocity", description = "By Kid .", category = ModuleCategory.COMBAT)
 class Velocity : Module() {
 
-    /**
-     * OPTIONS
-     */
-    private val horizontalValue = FloatValue("Horizontal", 0F, 0F, 1F)
-    private val verticalValue = FloatValue("Vertical", 0F, 0F, 1F)
-    private val modeValue = ListValue("Mode", arrayOf("Simple", "AAC", "AACPush", "AACZero",
-            "Reverse", "SmoothReverse", "Jump", "Glitch"), "Simple")
+    private val airticks = IntegerValue("AirTicks",2,0,10)
+    private val timerSpeed = FloatValue("TimerSpeed",0.49f,0.1f,1.0f)
+    private val airTest = BoolValue("AirTest",true)
+    private val jumpFix = BoolValue("JumpFix",true)
 
-    // Reverse
-    private val reverseStrengthValue = FloatValue("ReverseStrength", 1F, 0.1F, 1F)
-    private val reverse2StrengthValue = FloatValue("SmoothReverseStrength", 0.05F, 0.02F, 0.1F)
+    private var airtick = 0
+    private var air = false
+    private var pre = false
+    private var stop = false
 
-    // AAC Push
-    private val aacPushXZReducerValue = FloatValue("AACPushXZReducer", 2F, 1F, 3F)
-    private val aacPushYReducerValue = BoolValue("AACPushYReducer", true)
+    override fun onEnable() {
+        airtick = 0
+        air = false
+        air = false
+        pre = false
 
-    /**
-     * VALUES
-     */
-    private var velocityTimer = MSTimer()
-    private var velocityInput = false
-
-    // SmoothReverse
-    private var reverseHurt = false
-
-    // AACPush
-    private var jump = false
-
-    override val tag: String
-        get() = modeValue.get()
-
-    override fun onDisable() {
-        mc.thePlayer?.speedInAir = 0.02F
     }
 
-    @EventTarget
-    fun onUpdate(event: UpdateEvent) {
-        if (mc.thePlayer.isInWater || mc.thePlayer.isInLava || mc.thePlayer.isInWeb)
-            return
-
-        when (modeValue.get().toLowerCase()) {
-            "jump" -> if (mc.thePlayer.hurtTime > 0 && mc.thePlayer.onGround) {
-                mc.thePlayer.motionY = 0.42
-
-                val yaw = mc.thePlayer.rotationYaw * 0.017453292F
-                mc.thePlayer.motionX -= MathHelper.sin(yaw) * 0.2
-                mc.thePlayer.motionZ += MathHelper.cos(yaw) * 0.2
-            }
-
-            "glitch" -> {
-                mc.thePlayer.noClip = velocityInput
-                if (mc.thePlayer.hurtTime == 7)
-                    mc.thePlayer.motionY = 0.4
-
-                velocityInput = false
-            }
-
-            "reverse" -> {
-                if (!velocityInput)
-                    return
-
-                if (!mc.thePlayer.onGround) {
-                    MovementUtils.strafe(MovementUtils.getSpeed() * reverseStrengthValue.get())
-                } else if (velocityTimer.hasTimePassed(80L))
-                    velocityInput = false
-            }
-
-            "smoothreverse" -> {
-                if (!velocityInput) {
-                    mc.thePlayer.speedInAir = 0.02F
-                    return
-                }
-
-                if (mc.thePlayer.hurtTime > 0)
-                    reverseHurt = true
-
-                if (!mc.thePlayer.onGround) {
-                    if (reverseHurt)
-                        mc.thePlayer.speedInAir = reverse2StrengthValue.get()
-                } else if (velocityTimer.hasTimePassed(80L)) {
-                    velocityInput = false
-                    reverseHurt = false
-                }
-            }
-
-            "aac" -> if (velocityInput && velocityTimer.hasTimePassed(80L)) {
-                mc.thePlayer.motionX *= horizontalValue.get()
-                mc.thePlayer.motionZ *= horizontalValue.get()
-                //mc.thePlayer.motionY *= verticalValue.get() ?
-                velocityInput = false
-            }
-
-            "aacpush" -> {
-                if (jump) {
-                    if (mc.thePlayer.onGround)
-                        jump = false
-                } else {
-                    // Strafe
-                    if (mc.thePlayer.hurtTime > 0 && mc.thePlayer.motionX != 0.0 && mc.thePlayer.motionZ != 0.0)
-                        mc.thePlayer.onGround = true
-
-                    // Reduce Y
-                    if (mc.thePlayer.hurtResistantTime > 0 && aacPushYReducerValue.get()
-                            && !Memorial.moduleManager[Speed::class.java]!!.state)
-                        mc.thePlayer.motionY -= 0.014999993
-                }
-
-                // Reduce XZ
-                if (mc.thePlayer.hurtResistantTime >= 19) {
-                    val reduce = aacPushXZReducerValue.get()
-
-                    mc.thePlayer.motionX /= reduce
-                    mc.thePlayer.motionZ /= reduce
-                }
-            }
-
-            "aaczero" -> if (mc.thePlayer.hurtTime > 0) {
-                if (!velocityInput || mc.thePlayer.onGround || mc.thePlayer.fallDistance > 2F)
-                    return
-
-                mc.thePlayer.addVelocity(0.0, -1.0, 0.0)
-                mc.thePlayer.onGround = true
-            } else
-                velocityInput = false
-        }
+    override fun onDisable() {
+        mc.timer.timerSpeed = 1f
     }
 
     @EventTarget
     fun onPacket(event: PacketEvent) {
+        val blink = Memorial.moduleManager.getModule(Blink::class.java) as Blink
         val packet = event.packet
-
-        if (packet is S12PacketEntityVelocity) {
-            if (mc.thePlayer == null || (mc.theWorld?.getEntityByID(packet.entityID) ?: return) != mc.thePlayer)
-                return
-
-            velocityTimer.reset()
-
-            when (modeValue.get().toLowerCase()) {
-                "simple" -> {
-                    val horizontal = horizontalValue.get()
-                    val vertical = verticalValue.get()
-
-                    if (horizontal == 0F && vertical == 0F)
-                        event.cancelEvent()
-
-                    packet.motionX = (packet.getMotionX() * horizontal).toInt()
-                    packet.motionY = (packet.getMotionY() * vertical).toInt()
-                    packet.motionZ = (packet.getMotionZ() * horizontal).toInt()
-                }
-
-                "aac", "reverse", "smoothreverse", "aaczero" -> velocityInput = true
-
-                "glitch" -> {
-                    if (!mc.thePlayer.onGround)
-                        return
-
-                    velocityInput = true
-                    event.cancelEvent()
-                }
-            }
-        }
-
-        if (packet is S27PacketExplosion) {
-            // TODO: Support velocity for explosions
+        if(packet is S12PacketEntityVelocity && packet.entityID == mc.thePlayer.entityId && airTest.get()){
             event.cancelEvent()
+            airtick = airticks.get()
+        }
+        if(airtick > 0){
+            mc.timer.timerSpeed = timerSpeed.get()
+            if(packet is C04PacketPlayerPosition || packet is C06PacketPlayerPosLook){
+
+                mc.netHandler.addToSendQueue(C07PacketPlayerDigging(C07PacketPlayerDigging.Action.STOP_DESTROY_BLOCK,
+                    BlockPos(mc.thePlayer.posX,mc.thePlayer.posY,mc.thePlayer.posZ),EnumFacing.UP
+                ))
+                mc.netHandler.addToSendQueue(C03PacketPlayer(mc.thePlayer.onGround))
+                mc.netHandler.addToSendQueue(C07PacketPlayerDigging(C07PacketPlayerDigging.Action.STOP_DESTROY_BLOCK,
+                    BlockPos(mc.thePlayer.posX,mc.thePlayer.posY + 1,mc.thePlayer.posZ),EnumFacing.UP
+                ))
+
+            }
         }
     }
 
     @EventTarget
-    fun onJump(event: JumpEvent) {
-        if (mc.thePlayer == null || mc.thePlayer.isInWater || mc.thePlayer.isInLava || mc.thePlayer.isInWeb)
-            return
+    fun onUpdate(event: UpdateEvent){
 
-        when (modeValue.get().toLowerCase()) {
-            "aacpush" -> {
-                jump = true
+        if(airtick > 0){
+            air = true
 
-                if (!mc.thePlayer.isCollidedVertically)
-                    event.cancelEvent()
+            airtick--
+        }
+
+        if(air){
+            if(airtick < 1){
+                mc.timer.timerSpeed = 1f
+                air = false
             }
-            "aaczero" -> if (mc.thePlayer.hurtTime > 0)
+        }
+    }
+
+    @EventTarget
+    fun onTIck(event: TickEvent){
+        if(airtick > 0){
+            mc.netHandler.addToSendQueue(C07PacketPlayerDigging(C07PacketPlayerDigging.Action.STOP_DESTROY_BLOCK,
+                BlockPos(mc.thePlayer.posX,mc.thePlayer.posY,mc.thePlayer.posZ),EnumFacing.UP
+            ))
+        }
+    }
+
+    @EventTarget
+    fun onStrafe(event: StrafeEvent){
+
+    }
+
+    @EventTarget
+    fun onMotion(event: MotionEvent){
+        pre = !event.state.stateName.equals("Post",true)
+    }
+
+    @EventTarget
+    fun onJUmp(event: JumpEvent){
+        if(airtick > 0){
+            if(jumpFix.get())
                 event.cancelEvent()
         }
     }
+    override val tag: String?
+        get() = "Grim"
 }
